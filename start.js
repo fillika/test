af86da72111fe8c8ff66a57bd06f2e9229b5ef0d
Aleksandr Mironenko <alfie.mir@gmail.com>
2021-09-13 21:21:35 +0300

    MC-12345 create function with for loop

--- start.js
+++ start.js
@@ -6,3 +6,11 @@ const sum = (a, b) => a + b;
 const result = sum(10, 20);
 
 println(result)
+
+function goAwayForLoop(x) {
+    for (let i = 0; i < x; i++) {
+        println(x)
+    }
+}
+
+goAwayForLoop(100);
